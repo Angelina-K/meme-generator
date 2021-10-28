d1f4656e5bb688e92d086c6a10bf2d614f96f71b
Angelina k <ang92lm@gmail.com>
2021-10-28 12:03:32 +0300

    clicked line correctly

--- js/inputs-controller.js
+++ js/inputs-controller.js
@@ -1,4 +1,5 @@
 'use strict';
+let gStartPos;
 
 function onSelectImg(imgId) {
   updateMemeImg(imgId);
@@ -26,23 +27,51 @@ function onAddLine() {
 function onSelectLine() {
   changeLineFocus();
   changeCanvasContent();
-  // if (meme.lines[getCurrLineIdx()].isFocus) {
-  //   focusOnLine();
-  // } else {
-  //   changeCanvasContent();
-  // }
 }
-
-// toggleLineFocus();
-// const meme = getCurrMeme();
-// if (meme.lines[getCurrLineIdx()].isFocus) {
-//   focusOnLine();
-// } else {
-//   changeCanvasContent();
-// }
-// changeCanvasContent();
 
 function clearPlaceholder() {
   let elPlaceHolder = document.querySelector('.text-input');
   elPlaceHolder.value = elPlaceHolder.ariaPlaceholder;
+}
+
+function getEvPos(ev) {
+  var pos = {
+    x: ev.offsetX,
+    y: ev.offsetY,
+  };
+  // if (gTouchEvs.includes(ev.type)) {
+  //   ev.preventDefault();
+  //   ev = ev.changedTouches[0];
+  //   pos = {
+  //     x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+  //     y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+  //   };
+  // }
+  return pos;
+}
+
+function onDown(ev) {
+  console.log('onDown');
+  const pos = getEvPos(ev);
+  if (!isLineClicked(pos)) return;
+  setLineDrag(true);
+  gStartPos = pos;
+  // document.body.style.cursor = 'grabbing';
+}
+
+function onMove(ev) {
+  const circle = getCircle();
+  if (circle.isDrag) {
+    const pos = getEvPos(ev);
+    const dx = pos.x - gStartPos.x;
+    const dy = pos.y - gStartPos.y;
+    gStartPos = pos;
+    moveCircle(dx, dy);
+    renderCanvas();
+  }
+}
+
+function onUp() {
+  setLineDrag(false);
+  // document.body.style.cursor = 'grab';
 }
